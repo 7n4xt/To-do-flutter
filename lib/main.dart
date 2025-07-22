f3c0e984685f51b1282f7d254f20763564bd4947
Abdulmalek ESUGHI <abdul.esughi@gmail.com>
2025-07-22 15:35:25 +0200

    testing how a counter app works

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/pages/counter_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,42 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      // home: Scaffold(
-      //   body: GridView.builder(
-      //     itemCount: 64,
-      //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //       crossAxisCount: 18,
-      //     ),
-      //     itemBuilder: (context, index) =>
-      //         Container(color: Colors.deepPurple, margin: EdgeInsets.all(1.5)),
-      //   ),
-      // ),
-
-      // body: Stack(
-      //   alignment: Alignment.center,
-      //   children: [
-      //     Container(
-      //       height: 300,
-      //       width: 300,
-      //       color: const Color.fromARGB(255, 8, 8, 8),
-      //     ),
-      //     Container(
-      //       height: 200,
-      //       width: 200,
-      //       color: const Color.fromARGB(255, 56, 56, 56),
-      //     ),
-      //     Container(
-      //       height: 100,
-      //       width: 100,
-      //       color: const Color.fromARGB(255, 187, 187, 187),
-      //     ),
-      //   ],
-      // ),
-
-      home: Scaffold(
-        
-        ),
+      home: CounterPage(),
     );
   }
 }
